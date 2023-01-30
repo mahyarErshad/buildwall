7d7055d8e4ff4f81b5c8fb9c98b3c75869b2d28b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-30 14:20:20 +0330

    Before of navlinks are done

--- src/Components/Header/NavLink/NavLink.tsx
+++ src/Components/Header/NavLink/NavLink.tsx
@@ -9,8 +9,8 @@ interface props {
 function NavLink(props: props) {
   const { href, text } = props;
   return (
-    <li className={`${styles.wrapper} cursor-pointer h-full`}>
-      <a className="text-white font-semibold transition-all duration-300" href={href}>
+    <li className={`${styles.wrapper} cursor-pointer max-lg:before:hidden`}>
+      <a className="text-white font-semibold text-lg transition-all duration-300 inline-block max-lg:my-2" href={href}>
         {text}
       </a>
     </li>
