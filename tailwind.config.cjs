c52e67a3f2efdf173c0a1f443e88856062a3d47b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-30 10:25:50 +0330

    Created color variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#ffe618",
+      secondary: "#222930",
+      customGray: "#f5f5f1",
+      customBlack: "#222931",
+    },
   },
   plugins: [],
 };
