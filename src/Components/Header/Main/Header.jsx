4d4c85b5e5b5489b9a8b0597079758c2c9b75512
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-30 12:45:00 +0330

    Burger menu has an animation

--- src/Components/Header/Main/Header.jsx
+++ src/Components/Header/Main/Header.jsx
@@ -22,10 +22,10 @@ function Header() {
         <div className="flex items-center justify-between h-16">
           <div className="-mr-2 flex lg:hidden">
             <button onClick={() => setIsOpen((prev) => !prev)} type="button" className="bg-transparent flex-center p-2 rounded-md hover:text-primary transition-all duration-300" aria-controls="mobile-menu" aria-expanded="false">
-              <div className="flex-center flex-col gap-1">
-                <span className="h-1 w-[1.5625rem] bg-white"></span>
-                <span className="h-1 w-[1.5625rem] bg-white"></span>
-                <span className="h-1 w-[1.5625rem] bg-white"></span>
+              <div className="w-[1.5625rem] h-[1.5625rem] relative">
+                <span className={`h-1 w-full transition-all duration-500 bg-white block absolute ${!isOpen && "top-0"} ${isOpen && "rotate-[50deg] top-[10px]"}`}></span>
+                <span className={`h-1 w-full transition-all duration-500 bg-white block absolute top-[37%] ${isOpen && "opacity-0"}`}></span>
+                <span className={`h-1 w-full transition-all duration-500 bg-white block absolute ${!isOpen && "top-[18px]"} ${isOpen && "rotate-[-50deg] top-[10px]"}`}></span>
               </div>
             </button>
           </div>
