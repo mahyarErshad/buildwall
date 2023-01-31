56b247623d9026c5a565c58362f8b1523a0ee7b3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-31 10:37:33 +0330

    Target can be caught by props

--- src/Components/Utils/NeonButton/NeonButton.tsx
+++ src/Components/Utils/NeonButton/NeonButton.tsx
@@ -5,10 +5,11 @@ interface props {
   text: string;
   href?: string;
   className?: string;
+  target?: "_blank";
 }
 function NeonButton(props: props) {
   return (
-    <a className={`${styles.btn} ${props.className} max-md:bg-primary md:bg-transparent max-md:text-customBlack shadow-xl md:text-white`} href={props.href || "#"}>
+    <a target={props.target || ""} className={`${styles.btn} ${props.className} max-md:bg-primary md:bg-transparent max-md:text-customBlack shadow-xl md:text-white`} href={props.href || "#"}>
       <span></span>
       <span></span>
       <span></span>
