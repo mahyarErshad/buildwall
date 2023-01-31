72ad5dce82664f18fb404c355880e4a2746a0956
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-31 09:42:52 +0330

    Images are responsive

--- src/Components/Body/HeroSection/HeroSection.jsx
+++ src/Components/Body/HeroSection/HeroSection.jsx
@@ -8,7 +8,7 @@ function HeroSection() {
       <article className="mx-auto pt-20 flex-center flex-col gap-4">
         <h1 className="font-bold text-3xl">تم یکپارچه برای انواع مشاغل ساختمانی!</h1>
         <p className="text-xl">کسب و کار خود را با عملکرد باورنکردنی و رابط کاربری کارآمد تقویت کنید!</p>
-        <img className="max-w-[73.125rem] mt-8" src={headerImage} alt="Hero Image" />
+        <img className="w-full max-w-[73.125rem] mt-8" src={headerImage} alt="Hero Image" />
       </article>
     </section>
   );
