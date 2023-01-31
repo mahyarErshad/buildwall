5380d1f67c00fe0536ef934b348a967a481c8057
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-31 09:11:20 +0330

    Imported module styles

--- src/Components/Body/HeroSection/HeroSection.jsx
+++ src/Components/Body/HeroSection/HeroSection.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from "./styles.module.css"
 
 function HeroSection() {
   return (
-    <section className='mt-20 wrapper'>
+    <section className={`mt-20 ${styles.wrapper}`}>
 
     </section>
   )
