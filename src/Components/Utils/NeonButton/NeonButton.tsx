f06e6ed8deeb33f725f8f15ce8c4fec8fd61b341
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-31 10:15:56 +0330

    Button is fully responsive

--- src/Components/Utils/NeonButton/NeonButton.tsx
+++ src/Components/Utils/NeonButton/NeonButton.tsx
@@ -4,10 +4,11 @@ import styles from "./styles.module.css";
 interface props {
   text: string;
   href?: string;
+  className?: string;
 }
 function NeonButton(props: props) {
   return (
-    <a className={styles.btn} href={props.href || "#"}>
+    <a className={`${styles.btn} ${props.className} max-md:bg-primary md:bg-transparent max-md:text-customBlack shadow-xl md:text-white`} href={props.href || "#"}>
       <span></span>
       <span></span>
       <span></span>
