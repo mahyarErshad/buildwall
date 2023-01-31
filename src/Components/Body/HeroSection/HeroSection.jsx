e7ebe20e7001d00f1105e21888d0db1e3e7952df
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-31 09:41:41 +0330

    Worked on responsive

--- src/Components/Body/HeroSection/HeroSection.jsx
+++ src/Components/Body/HeroSection/HeroSection.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./styles.module.css";
-import headerImage from "../../../assets/images/headerFrontImage.webp"
+import headerImage from "../../../assets/images/headerFrontImage.webp";
 
 function HeroSection() {
   return (
     <section className={`${styles.wrapper} w-full h-full`}>
-      <article className="relative bottom-[28.125rem] mx-auto flex-center flex-col gap-4">
+      <article className="mx-auto pt-20 flex-center flex-col gap-4">
         <h1 className="font-bold text-3xl">تم یکپارچه برای انواع مشاغل ساختمانی!</h1>
         <p className="text-xl">کسب و کار خود را با عملکرد باورنکردنی و رابط کاربری کارآمد تقویت کنید!</p>
+        <img className="max-w-[73.125rem] mt-8" src={headerImage} alt="Hero Image" />
       </article>
     </section>
   );
