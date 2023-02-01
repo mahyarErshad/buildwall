3ad89cf6ae583ed66d83fc3581c3df4d4a1644e7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 10:27:41 +0330

    Cards have been created and are responsive

--- src/Components/Body/DemosSection/DemosSection.jsx
+++ src/Components/Body/DemosSection/DemosSection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import DemoSectionCard from "./DemosSectionCard";
 
 function DemosSection() {
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <section id="demos" className="max-md:mt-40 max-lg:mt-52 lg:mt-[17rem] w-full flex-center flex-col">
       <div className="flex-center max-md:flex-col gap-4 px-4 mb-12">
@@ -10,8 +12,10 @@ function DemosSection() {
           <h3 className="text-white text-sm">پوسته های منحصر به فرد را برای چندین موضوع ساخت و ساز در این یک موضوع پیدا کنید!</h3>
         </div>
       </div>
-      <section className="w-full">
-
+      <section className="w-full flex-center gap-6 flex-wrap">
+        {data.map((item, index) => {
+          return <DemoSectionCard key={index} title="ساخت و ساز" image="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" />;
+        })}
       </section>
     </section>
   );
