e235dcf05316020348b9be54228b8ff17820ab2e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 09:43:13 +0330

    Texts start at the beginning

--- src/Components/Body/DemosSection/DemosSection.jsx
+++ src/Components/Body/DemosSection/DemosSection.jsx
@@ -5,7 +5,7 @@ function DemosSection() {
     <section id="demos" className="max-md:mt-40 max-lg:mt-52 lg:mt-[17rem] w-full flex-center">
       <div className="flex-center max-md:flex-col gap-4 px-4">
         <h3 className="flex-center text-primary text-[5rem]">32</h3>
-        <div className="flex-center flex-col gap-4">
+        <div className="flex items-start justify-center flex-col gap-4">
           <h2 className="font-bold text-white text-3xl">دموی شگفت انگیز در انتظار شما هستند!</h2>
           <h3 className="text-white text-sm">پوسته های منحصر به فرد را برای چندین موضوع ساخت و ساز در این یک موضوع پیدا کنید!</h3>
         </div>
