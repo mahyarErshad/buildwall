7e1143bdf88a0dadd5e03ba7e73dac758367885b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 10:08:02 +0330

    Defined props

--- src/Components/Body/DemosSection/DemosSectionCard.tsx
+++ src/Components/Body/DemosSection/DemosSectionCard.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import NeonButton from "../../Utils/NeonButton/NeonButton";
 
-function DemosSectionCard() {
+interface props {
+  image: string;
+  title: string;
+}
+
+function DemosSectionCard(props: props) {
   return (
     <figure className="flex-center flex-col gap-4">
       <img src="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" alt="test" />
