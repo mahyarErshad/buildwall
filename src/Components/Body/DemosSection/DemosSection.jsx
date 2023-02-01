290c468d994762e607882c163509216b145de3aa
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 09:50:48 +0330

    Margin bottom is 50px

--- src/Components/Body/DemosSection/DemosSection.jsx
+++ src/Components/Body/DemosSection/DemosSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function DemosSection() {
   return (
     <section id="demos" className="max-md:mt-40 max-lg:mt-52 lg:mt-[17rem] w-full flex-center">
-      <div className="flex-center max-md:flex-col gap-4 px-4">
+      <div className="flex-center max-md:flex-col gap-4 px-4 mb-12">
         <h3 className="flex-center text-primary text-[5rem]">32</h3>
         <div className="flex items-start justify-center flex-col gap-4">
           <h2 className="font-bold text-white text-3xl">دموی شگفت انگیز در انتظار شما هستند!</h2>
