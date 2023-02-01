f5aa0a347cd69e2043a04752f87ed49bb62464f1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 14:19:23 +0330

    White space is no wrap

--- src/Components/Header/NavLink/NavLink.tsx
+++ src/Components/Header/NavLink/NavLink.tsx
@@ -10,7 +10,7 @@ function NavLink(props: props) {
   const { href, text } = props;
   return (
     <li className={`${styles.wrapper} cursor-pointer max-lg:before:hidden`}>
-      <a className="text-white font-semibold lg:hover:-translate-y-1 transition-all duration-300 inline-block max-lg:my-2" href={href}>
+      <a className="text-white whitespace-nowrap font-semibold lg:hover:-translate-y-1 transition-all duration-300 inline-block max-lg:my-2" href={href}>
         {text}
       </a>
     </li>
