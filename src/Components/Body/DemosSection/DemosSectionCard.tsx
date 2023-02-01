5de79252b1fcd28135230602e0cf6115d9893747
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 10:35:01 +0330

    Cards have zoom-in animation

--- src/Components/Body/DemosSection/DemosSectionCard.tsx
+++ src/Components/Body/DemosSection/DemosSectionCard.tsx
@@ -8,7 +8,7 @@ interface props {
 
 function DemosSectionCard(props: props) {
   return (
-    <figure className="flex-center flex-col gap-4 max-md:mb-8 md:mb-4 max-md:w-full md:w-[40%] lg:w-1/4">
+    <figure data-aos="zoom-in" className="flex-center flex-col gap-4 max-md:mb-8 md:mb-4 max-md:w-full md:w-[40%] lg:w-1/4">
       <img src={props.image} alt={props.title} />
       <h2 className="mb-4">{props.title}</h2>
       <NeonButton />
