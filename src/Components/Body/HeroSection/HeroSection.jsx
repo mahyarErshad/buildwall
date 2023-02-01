88ce664b95a13cb0d3e98fb198c8942393323e26
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 14:31:42 +0330

    Margin top has decreased

--- src/Components/Body/HeroSection/HeroSection.jsx
+++ src/Components/Body/HeroSection/HeroSection.jsx
@@ -8,11 +8,11 @@ function HeroSection() {
   return (
     <section className={`${styles.wrapper} w-full h-full`}>
       <article className="mx-auto pt-20 flex-center flex-col gap-4">
-        <TypeAnimation className="font-bold text-secondary text-3xl" sequence={["قالب شرکتی ساختمانی بیلدوال", 3000, "قالب یکپارچه برای انواع سایت های شرکتی!", 3000, , "قالب یکپارچه برای انواع مشاغل ساختمانی!", 3000]} speed={50} deletionSpeed={80} wrapper="h1" repeat={Infinity} />
+        <TypeAnimation className="font-bold text-secondary text-3xl max-sm:h-20 sm:h-auto" sequence={["قالب شرکتی ساختمانی بیلدوال", 3000, "قالب یکپارچه برای انواع سایت های شرکتی!", 3000, , "قالب یکپارچه برای انواع مشاغل ساختمانی!", 3000]} speed={50} deletionSpeed={80} wrapper="h1" repeat={Infinity} />
         <p className="text-xl text-secondary">کسب و کار خود را با عملکرد باورنکردنی و رابط کاربری کارآمد متحول کنید!</p>
         <img className="w-full max-w-[73.125rem] mt-8" src={headerImage} alt="Hero Image" />
       </article>
-      <NeonButton href="https://dana-team.com/products/buildwall/" target="_blank" className="max-sm:mt-32 max-md:mt-16 md:mt-8" />
+      <NeonButton href="https://dana-team.com/products/buildwall/" target="_blank" className="max-sm:mt-24 max-md:mt-16 md:mt-8" />
     </section>
   );
 }
