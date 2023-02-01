bf736dffd83017ef4ff55dc0769b697c75db9a31
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 10:05:56 +0330

    Paste the card here

---
+++ src/Components/Body/DemosSection/DemosSectionCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NeonButton from "../../Utils/NeonButton/NeonButton";
+
+function DemosSectionCard() {
+  return (
+    <figure className="flex-center flex-col gap-4">
+      <img src="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" alt="test" />
+      <h2 className="mb-4">ساخت و ساز</h2>
+      <NeonButton />
+    </figure>
+  );
+}
+
+export default DemosSectionCard;
