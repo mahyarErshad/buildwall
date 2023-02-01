c8f2fad88113cdbcdf43984a1ba0eb9e4a319772
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 10:09:03 +0330

    Component is dynamic

--- src/Components/Body/DemosSection/DemosSectionCard.tsx
+++ src/Components/Body/DemosSection/DemosSectionCard.tsx
@@ -9,8 +9,8 @@ interface props {
 function DemosSectionCard(props: props) {
   return (
     <figure className="flex-center flex-col gap-4">
-      <img src="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" alt="test" />
-      <h2 className="mb-4">ساخت و ساز</h2>
+      <img src={props.image} alt={props.title} />
+      <h2 className="mb-4">{props.title}</h2>
       <NeonButton />
     </figure>
   );
