f6effdb413e49ad18cbd8a8c42c95ebfbd092cf2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 14:02:19 +0330

    Using type animation

--- src/Components/Body/HeroSection/HeroSection.jsx
+++ src/Components/Body/HeroSection/HeroSection.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import styles from "./styles.module.css";
 import headerImage from "../../../assets/images/headerFrontImage.webp";
 import NeonButton from "../../Utils/NeonButton/NeonButton";
+import { TypeAnimation } from "react-type-animation";
 
 function HeroSection() {
   return (
     <section className={`${styles.wrapper} w-full h-full`}>
       <article className="mx-auto pt-20 flex-center flex-col gap-4">
-        <h1 className="font-bold text-secondary text-3xl">تم یکپارچه برای انواع مشاغل ساختمانی!</h1>
-        <p className="text-xl text-secondary">کسب و کار خود را با عملکرد باورنکردنی و رابط کاربری کارآمد تقویت کنید!</p>
+        <TypeAnimation className="font-bold text-secondary text-3xl" sequence={["قالب شرکتی ساختمانی بیلدوال", 3000, "قالب یکپارچه برای انواع سایت های شرکتی!", 3000, , "قالب یکپارچه برای انواع مشاغل ساختمانی!", 3000]} speed={50} deletionSpeed={80} wrapper="h1" repeat={Infinity} />
+        <p className="text-xl text-secondary">کسب و کار خود را با عملکرد باورنکردنی و رابط کاربری کارآمد متحول کنید!</p>
         <img className="w-full max-w-[73.125rem] mt-8" src={headerImage} alt="Hero Image" />
       </article>
       <NeonButton href="https://dana-team.com/products/buildwall/" target="_blank" className="max-sm:mt-28 max-md:mt-24 md:mt-8" />
