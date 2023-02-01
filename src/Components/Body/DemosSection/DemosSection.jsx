8ea0b37d89134914ef829823e35993216011c228
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 09:25:37 +0330

    Text section has been added

---
+++ src/Components/Body/DemosSection/DemosSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function DemosSection() {
+  return (
+    <section id="demos" className="mt-20 ">
+      <div className="flex-center gap-4 max-w-[33.75rem] flex-wrap">
+        <h3 className="flex-center text-primary max-md:text-[2.5rem] md:text-[5rem]">32</h3>
+        <div className="flex-center flex-col gap-4">
+          <h2 className="font-bold text-3xl">دموهای شگفت انگیز در انتظار شما هستند!</h2>
+          <h3 className="text-[#5f656d] text-sm">پوسته های منحصر به فرد را برای چندین موضوع ساخت و ساز در این یک موضوع پیدا کنید!</h3>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default DemosSection;
