3721346ae364d706c8385fa3ca0a967c0ffff7d4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-01 09:46:11 +0330

    Texts are custom black

--- src/Components/Body/HeroSection/HeroSection.jsx
+++ src/Components/Body/HeroSection/HeroSection.jsx
@@ -7,8 +7,8 @@ function HeroSection() {
   return (
     <section className={`${styles.wrapper} w-full h-full`}>
       <article className="mx-auto pt-20 flex-center flex-col gap-4">
-        <h1 className="font-bold text-3xl">تم یکپارچه برای انواع مشاغل ساختمانی!</h1>
-        <p className="text-xl">کسب و کار خود را با عملکرد باورنکردنی و رابط کاربری کارآمد تقویت کنید!</p>
+        <h1 className="font-bold text-customBlack text-3xl">تم یکپارچه برای انواع مشاغل ساختمانی!</h1>
+        <p className="text-xl text-customBlack">کسب و کار خود را با عملکرد باورنکردنی و رابط کاربری کارآمد تقویت کنید!</p>
         <img className="w-full max-w-[73.125rem] mt-8" src={headerImage} alt="Hero Image" />
       </article>
       <NeonButton href="https://dana-team.com/products/buildwall/" target="_blank" className="max-sm:mt-28 max-md:mt-24 md:mt-8" text="مشاهده دموها" />
