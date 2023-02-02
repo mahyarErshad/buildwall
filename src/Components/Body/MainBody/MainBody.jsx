15f53d5653044f4a8818ecf17598308240f4431d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-02 09:10:00 +0330

    Imported ElementorAndPlugins

--- src/Components/Body/MainBody/MainBody.jsx
+++ src/Components/Body/MainBody/MainBody.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DemosSection from "../DemosSection/DemosSection";
+import ElementorAndPlugins from "../ElementorAndPlugins/ElementorAndPlugins";
 import HeroSection from "../HeroSection/HeroSection";
 
 function MainBody() {
@@ -7,7 +8,7 @@ function MainBody() {
     <main className="relative max-md:pb-8 md:pb-12">
       <HeroSection />
       <DemosSection />
-      
+      <ElementorAndPlugins />
     </main>
   );
 }
