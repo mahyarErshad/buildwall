056fde59e638e4eac5bb4f574d762d27018dcbc9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-02 09:09:34 +0330

    Added title text

--- src/Components/Body/ElementorAndPlugins/ElementorAndPlugins.jsx
+++ src/Components/Body/ElementorAndPlugins/ElementorAndPlugins.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react'
 
 function ElementorAndPlugins() {
-  return <div>ElementorAndPlugins</div>;
+  return (
+    <section className='w-full mt-20 flex-center flex-col'>
+    <div className='flex-center flex-col w-full'>
+    <h2 className='text-3xl font-bold mb-5'>ساخته شده توسط المنتور</h2>
+    
+    </div>
+    </section>
+  )
 }
 
-export default ElementorAndPlugins;
+export default ElementorAndPlugins
