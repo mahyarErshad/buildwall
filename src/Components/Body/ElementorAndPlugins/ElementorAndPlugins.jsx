9e780c7583531d85b53880dfa1dbe7eef6aec04e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-02 09:27:13 +0330

    Imported BuiltWithElementor

--- src/Components/Body/ElementorAndPlugins/ElementorAndPlugins.jsx
+++ src/Components/Body/ElementorAndPlugins/ElementorAndPlugins.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
+import BuiltWithElementor from "./Cards/BuiltWithElementor";
 
 function ElementorAndPlugins() {
   return (
-    <section className='w-full mt-20 flex-center flex-col'>
-    <div className='flex-center flex-col w-full'>
-    <h2 className='text-3xl font-bold mb-5'>ساخته شده توسط المنتور</h2>
-    <ul>
-        
-    </ul>
-    </div>
+    <section className="w-full mt-20 flex-center flex-col">
+      <div className="flex-center flex-col w-full">
+        <h2 className="text-3xl font-bold mb-8">ساخته شده توسط المنتور</h2>
+        <ul>
+          <BuiltWithElementor text="محتوا را بدون مهارت کد زدن اضافه کنید" />
+        </ul>
+      </div>
     </section>
-  )
+  );
 }
 
-export default ElementorAndPlugins
+export default ElementorAndPlugins;
