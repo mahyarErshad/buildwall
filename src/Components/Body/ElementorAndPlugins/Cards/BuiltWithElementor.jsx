659e45b50d20450614414ab9687dcff016334fd6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-02 09:21:42 +0330

    Added icon

--- src/Components/Body/ElementorAndPlugins/Cards/BuiltWithElementor.jsx
+++ src/Components/Body/ElementorAndPlugins/Cards/BuiltWithElementor.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 function BuiltWithElementor() {
   return (
-    <div>BuiltWithElementor</div>
-  )
+    <div className="flex-center gap-3">
+      <FontAwesomeIcon className="text-primary" icon={faCheck} />
+      <h3></h3>
+    </div>
+  );
 }
 
-export default BuiltWithElementor
+export default BuiltWithElementor;
