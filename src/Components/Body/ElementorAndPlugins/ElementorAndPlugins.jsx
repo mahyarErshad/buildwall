36606f8a27bb32798b4f0311ee255f3c4855e1c4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-02 09:55:34 +0330

    BuiltWithElementor cards are done

--- src/Components/Body/ElementorAndPlugins/ElementorAndPlugins.jsx
+++ src/Components/Body/ElementorAndPlugins/ElementorAndPlugins.jsx
@@ -4,10 +4,12 @@ import BuiltWithElementor from "./Cards/BuiltWithElementor";
 function ElementorAndPlugins() {
   return (
     <section className="w-full mt-20 flex-center flex-col">
-      <div className="flex-center flex-col w-full">
-        <h2 className="text-3xl font-bold mb-8">ساخته شده توسط المنتور</h2>
-        <ul>
+      <div className="flex-center flex-col w-full gap-8">
+        <h2 className="text-3xl font-bold">ساخته شده توسط المنتور</h2>
+        <ul className="flex justify-center items-start flex-col gap-2">
           <BuiltWithElementor text="محتوا را بدون مهارت کد زدن اضافه کنید" />
+          <BuiltWithElementor text="در چشم به هم زدنی استایل ها را عوض کنید" />
+          <BuiltWithElementor text="بیش از 50 ویجت اضافی" />
         </ul>
       </div>
     </section>
