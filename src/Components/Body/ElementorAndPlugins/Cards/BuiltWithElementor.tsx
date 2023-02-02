ea2a1f8df4ba3ad3c0014460e24ac10adf51b30e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-02 09:22:31 +0330

    Defined props

--- src/Components/Body/ElementorAndPlugins/Cards/BuiltWithElementor.tsx
+++ src/Components/Body/ElementorAndPlugins/Cards/BuiltWithElementor.tsx
@@ -2,7 +2,11 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-function BuiltWithElementor() {
+interface props{
+  text: string;
+}
+
+function BuiltWithElementor(props:props) {
   return (
     <div className="flex-center gap-3">
       <FontAwesomeIcon className="text-primary" icon={faCheck} />
