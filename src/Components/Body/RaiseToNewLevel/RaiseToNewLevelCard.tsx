4c4b2101c2df3ba3f720b0cc047eee8dd6697923
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-05 10:29:44 +0330

    Cut the codes

--- src/Components/Body/RaiseToNewLevel/RaiseToNewLevelCard.tsx
+++ src/Components/Body/RaiseToNewLevel/RaiseToNewLevelCard.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 function RaiseToNewLevelCard() {
   return (
-    <div>RaiseToNewLevelCard</div>
-  )
+    <article className="flex-center w-full gap-8 flex-wrap mt-4">
+      <div className="flex-center flex-col max-md:w-full md:w-[35%]">
+        <FontAwesomeIcon className="w-10 h-10" fill="#ffe618" icon={faScrewdriverWrench} />
+        <h4 className="text-[1.375rem] font-bold mt-4 mb-6">خدمات ویترینی</h4>
+        <p className="text-sm">خدمات خود را به جذاب ترین شکل نمایش دهید و اطلاعات کاملی در مورد آنچه شرکت شما انجام می دهد به بازدیدکنندگان خود ارائه دهید. طراحی جذاب خدمات هیچ کس را بی تفاوت نمی گذارد!</p>
+      </div>
+      <img className="max-md:w-full md:w-[60%] max-w-[41.875rem]" src={showCaseServices} alt="show case services" />
+    </article>
+  );
 }
 
-export default RaiseToNewLevelCard
+export default RaiseToNewLevelCard;
