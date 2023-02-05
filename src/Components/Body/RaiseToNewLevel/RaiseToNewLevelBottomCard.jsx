b85bec80caf1d701a00aa51f1b967b1610e8b508
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-05 13:05:13 +0330

    Cards are aligned vertically

--- src/Components/Body/RaiseToNewLevel/RaiseToNewLevelBottomCard.jsx
+++ src/Components/Body/RaiseToNewLevel/RaiseToNewLevelBottomCard.jsx
@@ -6,7 +6,7 @@ function RaiseToNewLevelBottomCard({ title, description, icon }) {
     <article className="max-md:w-full md:max-w-[23.125rem] md:w-[30%] flex justify-start items-center flex-col gap-5">
       <FontAwesomeIcon icon={icon} className="w-10 h-10" fill="#ffe618" />
       <h3 className="text-[1.375rem] font-bold">{title}</h3>
-      <p className="text-sm text-center">{description}</p>
+      <p className="text-sm text-center min-h-[5rem]">{description}</p>
     </article>
   );
 }
