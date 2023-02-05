75eff362b2634b3b5bd844a5b07388a3007b52db
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-05 10:35:27 +0330

    Component is dynamic

---
+++ src/Components/Body/RaiseToNewLevel/RaiseToNewLevelCard.jsx
@@ -0,0 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+function RaiseToNewLevelCard({ icon, title, description, image, reverse }) {
+  const direction = reverse ? "flex-row-reverse" : "";
+  return (
+    <article className={`flex-center w-full gap-8 flex-wrap mt-4 ${direction}`}>
+      <div className="flex-center flex-col max-md:w-full md:w-[35%]">
+        <FontAwesomeIcon className="w-10 h-10" fill="#ffe618" icon={icon} />
+        <h4 className="text-[1.375rem] font-bold mt-4 mb-6">{title}</h4>
+        <p className="text-sm">{description}</p>
+      </div>
+      <img className="max-md:w-full md:w-[60%] max-w-[41.875rem]" src={image} alt={title} />
+    </article>
+  );
+}
+
+export default RaiseToNewLevelCard;
