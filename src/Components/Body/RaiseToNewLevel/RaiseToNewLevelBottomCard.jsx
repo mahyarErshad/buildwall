84da33e3a5ffba92cca2cac51b395702904fa7fc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-05 11:07:00 +0330

    Copied the codes

--- src/Components/Body/RaiseToNewLevel/RaiseToNewLevelBottomCard.jsx
+++ src/Components/Body/RaiseToNewLevel/RaiseToNewLevelBottomCard.jsx
@@ -1,8 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 function RaiseToNewLevelBottomCard() {
   return (
-    <div>RaiseToNewLevelBottomCard</div>
+    <article className="max-md:w-full md:max-w-[23.125rem] flex justify-start items-center flex-col gap-5">
+    <FontAwesomeIcon icon={faScrewdriverWrench} className="w-10 h-10" fill="#ffe618" />
+    <h3 className="text-[1.375rem] font-bold">عنوان</h3>
+    <p className="text-center text-sm">متن</p>
+  </article>
   )
 }
 
