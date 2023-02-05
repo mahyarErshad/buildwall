bccf64f153da3d0df73cb89691d7c1e3661fbd74
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-05 16:24:51 +0330

    Changed the icon

--- src/Components/Body/RaiseToNewLevel/RaiseToNewLevel.jsx
+++ src/Components/Body/RaiseToNewLevel/RaiseToNewLevel.jsx
@@ -1,4 +1,4 @@
-import { faScrewdriverWrench, faBuilding, faCartShopping, faCheckToSlot, faThumbsUp, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faScrewdriverWrench, faBuilding, faCartShopping, faThumbsUp, faUser, faMoneyBill1 } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import showCaseServices from "../../../assets/images/showCaseServices.webp";
 import masterYourProject from "../../../assets/images/masterYourProject.webp";
@@ -18,7 +18,7 @@ function RaiseToNewLevel() {
       <RaiseToNewLevelCard reverse title="به پروژه های خود مسلط شوید" description="بیلدوال به پروژه های پیشرفته مجهز شده است، پروژه های شما را به نمایش می گذارد و نقاط قوت شرکت ساخت و ساز شما را نشان می دهد. شما می توانید طرح بندی پروژه ها را با توجه به خواسته های خود انتخاب کنید." icon={faBuilding} image={masterYourProject} />
       <RaiseToNewLevelCard title="فروش موثر با ووکامرس" description="هیچ چیز ساده تر از فروش آنلاین با بسته ووکامرس بیلدوال نیست! کالاهای خود را اضافه کنید، دسته بندی های خود را ایجاد کنید و سود را با فروشگاه الکترونیکی یکپارچه به حداکثر برسانید!" icon={faCartShopping} image={sellWithWoocommerce} />
       <div className="w-full py-12 flex justify-between items-center flex-wrap">
-        <RaiseToNewLevelBottomCard title="لیست قیمت ها" description="این گزینه درجه یک امکان ایجاد و سفارشی کردن لیست قیمت ها را بدون دردسر فراهم می کند. شما قادر خواهید بود به راحتی و بدون نگرانی لیست قیمت های خود را تنظیم و تغییراتی ایجاد کنید!" icon={faCheckToSlot} />
+        <RaiseToNewLevelBottomCard title="لیست قیمت ها" description="این گزینه درجه یک امکان ایجاد و سفارشی کردن لیست قیمت ها را بدون دردسر فراهم می کند. شما قادر خواهید بود به راحتی و بدون نگرانی لیست قیمت های خود را تنظیم و تغییراتی ایجاد کنید!" icon={faMoneyBill1} />
         <RaiseToNewLevelBottomCard title="توصیفات را به نمایش بگذارید" description="شما فرصتی عالی برای نمایش بازخوردهای مشتریان راضی خود دارید و تصویر معتبر شرکت خود را برای بازدیدکنندگان جدید شکل می دهید. کلمات خوب خیلی چیزها را می گویند!" icon={faThumbsUp} />
         <RaiseToNewLevelBottomCard title="اعضای تیم را معرفی کنید" description="با بیلدوال می‌توانید اطلاعات دقیق و جذابی را در مورد کارمندان به بازدیدکنندگان خود ارائه دهید و کارهایی که انجام می‌دهند و مهارت‌ها، تجربه و قدرت آنها را برجسته کنید." icon={faUser} />
       </div>
