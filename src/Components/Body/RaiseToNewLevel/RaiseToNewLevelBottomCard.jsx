0131be240370e81666eb2e5a214ef6e27e4b6645
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-05 11:08:49 +0330

    Icon comes from props

--- src/Components/Body/RaiseToNewLevel/RaiseToNewLevelBottomCard.jsx
+++ src/Components/Body/RaiseToNewLevel/RaiseToNewLevelBottomCard.jsx
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-function RaiseToNewLevelBottomCard({ title, description }) {
+function RaiseToNewLevelBottomCard({ title, description, icon }) {
   return (
     <article className="max-md:w-full md:max-w-[23.125rem] flex justify-start items-center flex-col gap-5">
-      <FontAwesomeIcon icon={faScrewdriverWrench} className="w-10 h-10" fill="#ffe618" />
+      <FontAwesomeIcon icon={icon} className="w-10 h-10" fill="#ffe618" />
       <h3 className="text-[1.375rem] font-bold">{title}</h3>
       <p className="text-center text-sm">{description}</p>
     </article>
