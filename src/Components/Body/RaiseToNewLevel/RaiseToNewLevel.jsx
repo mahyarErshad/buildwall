2b96a45eab0d90316c9091f4f6c82d3d669040e8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-05 09:28:54 +0330

    Added article

--- src/Components/Body/RaiseToNewLevel/RaiseToNewLevel.jsx
+++ src/Components/Body/RaiseToNewLevel/RaiseToNewLevel.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 function RaiseToNewLevel() {
-  return <section className="max-w-[75rem] flex-center flex-col mt-10">
-  <div></div>
-  </section>;
+  return (
+    <section className="max-w-[75rem] flex-center flex-col mt-10 mx-auto px-2">
+      <article className="flex-center flex-col gap-4">
+        <h2 className="font-bold text-3xl">ارتقا به سطح بالاتر</h2>
+        <h3 className="text-[1.375rem] text-center">کاوش و پیاده سازی فرصت های بی پایان!</h3>
+        <p className="text-center text-sm">این تم دارای هر چیزی است که ممکن است برای اجرای آنلاین مشاغل ساختمانی خود به آن نیاز داشته باشید. این تم همه چیز را برای ارائه عملکرد قدرتمند و رابط کاربری واضح به شما ارائه میکند.</p>
+      </article>
+    </section>
+  );
 }
 
 export default RaiseToNewLevel;
