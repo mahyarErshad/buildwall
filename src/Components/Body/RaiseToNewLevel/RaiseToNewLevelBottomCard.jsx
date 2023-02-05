167681321aba5fcabeb8552bdfcaed4631cb20da
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-05 11:07:33 +0330

    Card is dynamic

--- src/Components/Body/RaiseToNewLevel/RaiseToNewLevelBottomCard.jsx
+++ src/Components/Body/RaiseToNewLevel/RaiseToNewLevelBottomCard.jsx
@@ -1,14 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-function RaiseToNewLevelBottomCard() {
+function RaiseToNewLevelBottomCard({ title, description }) {
   return (
     <article className="max-md:w-full md:max-w-[23.125rem] flex justify-start items-center flex-col gap-5">
-    <FontAwesomeIcon icon={faScrewdriverWrench} className="w-10 h-10" fill="#ffe618" />
-    <h3 className="text-[1.375rem] font-bold">عنوان</h3>
-    <p className="text-center text-sm">متن</p>
-  </article>
-  )
+      <FontAwesomeIcon icon={faScrewdriverWrench} className="w-10 h-10" fill="#ffe618" />
+      <h3 className="text-[1.375rem] font-bold">{title}</h3>
+      <p className="text-center text-sm">{description}</p>
+    </article>
+  );
 }
 
-export default RaiseToNewLevelBottomCard
+export default RaiseToNewLevelBottomCard;
