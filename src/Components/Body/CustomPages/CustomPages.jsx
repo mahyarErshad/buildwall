caadc01ed76a0b2fcb342b7303995b5891c4e5fa
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 08:53:05 +0330

    SectionTitle is created

--- src/Components/Body/CustomPages/CustomPages.jsx
+++ src/Components/Body/CustomPages/CustomPages.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import SectionTitle from "../../Utils/SectionTitle/SectionTitle";
 
 function CustomPages() {
-  return <section className="pt-20 w-full px-4">
-    
-  </section>;
+  return (
+    <section className="pt-20 w-full px-4">
+      <SectionTitle number={30} title="صفحات سفارشی" description="عملکرد و تنوع غنی بیلدوال را کاوش کنید" />
+    </section>
+  );
 }
 
 export default CustomPages;
