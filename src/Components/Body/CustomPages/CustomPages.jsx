facb92b3527dd0b9192edf3bc956dc90f3b50f3f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 09:18:00 +0330

    Text is center

--- src/Components/Body/CustomPages/CustomPages.jsx
+++ src/Components/Body/CustomPages/CustomPages.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import SectionTitle from "../../Utils/SectionTitle/SectionTitle";
+import NonButtonCard from "../../Utils/NonButtonCard/NonButtonCard";
 
 function CustomPages() {
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <section className="w-full mt-20 flex-center flex-col">
       <SectionTitle number={30} title="صفحه سفارشی" description="عملکرد و تنوع غنی بیلدوال را کاوش کنید" />
+      <section className="max-w-[75rem] flex-center gap-6 flex-wrap">
+        {data.map((item, index) => {
+          return <NonButtonCard key={index} title="ساخت و ساز" image="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" />;
+        })}
+      </section>
     </section>
   );
 }
--- src/Components/Utils/NonButtonCard/NonButtonCard.tsx
+++ src/Components/Utils/NonButtonCard/NonButtonCard.tsx
@@ -10,7 +10,7 @@ function NonButtonCard(props: props) {
   return (
     <a href={props.href || "#"} className="lg:w-[16.875rem] md:w-[45%] max-md:w-full flex-center flex-col gap-5">
       <img className="w-full" src={props.image} alt={props.title} />
-      <h3 className="w-full">{props.title}</h3>
+      <h3 className="w-full text-center">{props.title}</h3>
     </a>
   );
 }
