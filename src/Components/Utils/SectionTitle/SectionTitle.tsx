c1d9a15abd2b0b88d4749b42dc0ce604849cb591
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 08:40:53 +0330

    Using props

--- src/Components/Utils/SectionTitle/SectionTitle.tsx
+++ src/Components/Utils/SectionTitle/SectionTitle.tsx
@@ -10,10 +10,10 @@ function SectionTitle(props: props) {
     const {number, title, description} = props
   return (
     <div className="flex-center max-md:flex-col gap-4 px-4 mb-12">
-    <h3 className="flex-center text-primary text-[5rem]">32</h3>
+    <h3 className="flex-center text-primary text-[5rem]">{number}</h3>
     <div className="flex max-md:items-center md:items-start justify-center flex-col gap-4">
-      <h2 className="font-bold text-white text-3xl max-md:text-center">دموی شگفت انگیز در انتظار شما هستند!</h2>
-      <h3 className="text-white text-sm">پوسته های منحصر به فرد را برای چندین موضوع ساخت و ساز در این یک موضوع پیدا کنید!</h3>
+      <h2 className="font-bold text-white text-3xl max-md:text-center">{title}</h2>
+      <h3 className="text-white text-sm">{description}</h3>
     </div>
   </div>
   )
