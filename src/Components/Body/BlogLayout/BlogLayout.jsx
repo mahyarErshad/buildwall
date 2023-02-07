d3f36af99cf6e3b2930564b5856bca6ff04ab88e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 09:23:00 +0330

    Component initialized

--- src/Components/Body/BlogLayout/BlogLayout.jsx
+++ src/Components/Body/BlogLayout/BlogLayout.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import SectionTitle from "../../Utils/SectionTitle/SectionTitle";
+import NonButtonCard from "../../Utils/NonButtonCard/NonButtonCard";
 
 function BlogLayout() {
+  const data = [1, 2, 3, 4];
   return (
-    <div>BlogLayout</div>
-  )
+    <section className="w-full mt-20 flex-center flex-col">
+      <SectionTitle number={3} title="صفحه سفارشی" description="عملکرد و تنوع غنی بیلدوال را کاوش کنید" />
+      <section className="max-w-[75rem] flex-center gap-6 flex-wrap">
+        {data.map((item, index) => {
+          return <NonButtonCard key={index} title="ساخت و ساز" image="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" />;
+        })}
+      </section>
+    </section>
+  );
 }
 
-export default BlogLayout
+export default BlogLayout;
