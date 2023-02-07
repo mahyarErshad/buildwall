4814ef6027f5d90283cdc2b105bf3027fd55fbce
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 09:24:49 +0330

    Component is finished

--- src/Components/Body/BlogLayout/BlogLayout.jsx
+++ src/Components/Body/BlogLayout/BlogLayout.jsx
@@ -6,7 +6,7 @@ function BlogLayout() {
   const data = [1, 2, 3, 4];
   return (
     <section className="w-full mt-20 flex-center flex-col">
-      <SectionTitle number={3} title="صفحه سفارشی" description="عملکرد و تنوع غنی بیلدوال را کاوش کنید" />
+      <SectionTitle number={4} title="طرح بندی زیبای وبلاگ!" description="اخبار و پست های وبلاگ خود را به روش های مختلف نشان دهید!" />
       <section className="max-w-[75rem] flex-center gap-6 flex-wrap">
         {data.map((item, index) => {
           return <NonButtonCard key={index} title="ساخت و ساز" image="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" />;
