11e097574f7f94594b74dbefb90dc400fa3c04ef
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 09:37:03 +0330

    Added Cards

--- src/Components/Body/ShopLayout/ShopLayout.jsx
+++ src/Components/Body/ShopLayout/ShopLayout.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import NonButtonCard from "../../Utils/NonButtonCard/NonButtonCard";
 
 function ShopLayout() {
+  const data = [1, 2];
   return (
     <section className="w-full mt-20 flex-center flex-col">
       <h2 className="font-bold text-white text-3xl text-center">چیدمان شگفت انگیز فروشگاه روی میز!</h2>
       <p className="text-white text-center text-sm">فروش مقرون به صرفه و عاقلانه با گزینه های داخلی فروشگاه الکترونیکی!</p>
+      <section className="max-w-[75rem] flex-center gap-6 flex-wrap">
+        {data.map((item, index) => {
+          return <NonButtonCard key={index} title="ساخت و ساز" image="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" />;
+        })}
+      </section>
     </section>
   );
 }
