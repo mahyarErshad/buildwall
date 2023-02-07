9906d3a480d9257393e3c1780f60b34b61b88863
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 09:39:02 +0330

    Margin top is 3 rem

--- src/Components/Body/ShopLayout/ShopLayout.jsx
+++ src/Components/Body/ShopLayout/ShopLayout.jsx
@@ -7,7 +7,7 @@ function ShopLayout() {
     <section className="w-full mt-20 flex-center flex-col">
       <h2 className="font-bold text-white text-3xl text-center">چیدمان شگفت انگیز فروشگاه روی میز!</h2>
       <p className="text-white text-center text-sm">فروش مقرون به صرفه و عاقلانه با گزینه های داخلی فروشگاه الکترونیکی!</p>
-      <section className="max-w-[75rem] flex-center gap-6 flex-wrap">
+      <section className="max-w-[75rem] flex-center gap-6 flex-wrap mt-12">
         {data.map((item, index) => {
           return <NonButtonCard key={index} title="ساخت و ساز" image="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" />;
         })}
