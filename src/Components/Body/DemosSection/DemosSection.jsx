3dc8819584201494e48273bcf5470de71b40f731
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 08:47:01 +0330

    Using dynamic SectionTitle for cleaner code

--- src/Components/Body/DemosSection/DemosSection.jsx
+++ src/Components/Body/DemosSection/DemosSection.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import DemoSectionCard from "./DemosSectionCard";
+import SectionTitle from "../../Utils/SectionTitle/SectionTitle";
 
 function DemosSection() {
   const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <section id="demos" className="max-md:mt-40 max-lg:mt-52 lg:mt-[17rem] w-full flex-center flex-col">
-      <div className="flex-center max-md:flex-col gap-4 px-4 mb-12">
-        <h3 className="flex-center text-primary text-[5rem]">32</h3>
-        <div className="flex max-md:items-center md:items-start justify-center flex-col gap-4">
-          <h2 className="font-bold text-white text-3xl max-md:text-center">دموی شگفت انگیز در انتظار شما هستند!</h2>
-          <h3 className="text-white text-sm">پوسته های منحصر به فرد را برای چندین موضوع ساخت و ساز در این یک موضوع پیدا کنید!</h3>
-        </div>
-      </div>
+      <SectionTitle number="32" title="دموی شگفت انگیز در انتظار شما هستند!" description="پوسته های منحصر به فرد را برای چندین موضوع ساخت و ساز در این یک موضوع پیدا کنید!" />
       <section className="w-full flex-center gap-6 flex-wrap">
         {data.map((item, index) => {
           return <DemoSectionCard key={index} title="ساخت و ساز" image="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" />;
--- src/Components/Utils/SectionTitle/SectionTitle.tsx
+++ src/Components/Utils/SectionTitle/SectionTitle.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 
 interface props {
-    number: string;
-    title: string;
-    description: string;
+  number: string | number;
+  title: string;
+  description: string;
 }
 
 function SectionTitle(props: props) {
-    const {number, title, description} = props
+  const { number, title, description } = props;
   return (
     <div className="flex-center max-md:flex-col gap-4 px-4 mb-12">
-    <h3 className="flex-center text-primary text-[5rem]">{number}</h3>
-    <div className="flex max-md:items-center md:items-start justify-center flex-col gap-4">
-      <h2 className="font-bold text-white text-3xl max-md:text-center">{title}</h2>
-      <h3 className="text-white text-sm">{description}</h3>
+      <h3 className="flex-center text-primary text-[5rem]">{number}</h3>
+      <div className="flex max-md:items-center md:items-start justify-center flex-col gap-4">
+        <h2 className="font-bold text-white text-3xl max-md:text-center">{title}</h2>
+        <h3 className="text-white text-sm">{description}</h3>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default SectionTitle
+export default SectionTitle;
