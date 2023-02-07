17d1ce6674fbd2c6c899861120825f5027d1f3cc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 10:40:48 +0330

    Image has transition

--- src/Components/Utils/NonButtonCard/NonButtonCard.tsx
+++ src/Components/Utils/NonButtonCard/NonButtonCard.tsx
@@ -9,7 +9,7 @@ interface props {
 function NonButtonCard(props: props) {
   return (
     <a data-aos="zoom-in" href={props.href || "#"} className="lg:w-[16.875rem] NonButtonCard md:w-[45%] max-md:w-full max-w-[25rem] flex-center flex-col gap-5 transition-all duration-300 hover:-translate-y-2 hover:text-primary">
-      <img className="w-full" src={props.image} alt={props.title} />
+      <img className="w-full transition-all duration-300" src={props.image} alt={props.title} />
       <h3 className="w-full text-center text-inherit">{props.title}</h3>
     </a>
   );
