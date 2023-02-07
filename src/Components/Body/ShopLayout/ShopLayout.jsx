a1375ce883e0a3a3284d3ff025f6814fa1a1261d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 09:35:36 +0330

    Added 2 headings

--- src/Components/Body/ShopLayout/ShopLayout.jsx
+++ src/Components/Body/ShopLayout/ShopLayout.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function ShopLayout() {
   return (
     <section className="w-full mt-20 flex-center flex-col">
-        
+      <h2 className="font-bold text-white text-3xl text-center">چیدمان شگفت انگیز فروشگاه روی میز!</h2>
+      <h3 className="text-white text-center text-sm">فروش مقرون به صرفه و عاقلانه با گزینه های داخلی فروشگاه الکترونیکی!</h3>
     </section>
-  )
+  );
 }
 
-export default ShopLayout
+export default ShopLayout;
