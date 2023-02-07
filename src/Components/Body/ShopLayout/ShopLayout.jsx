2760464589e1d5c99cfc807b21c3e0631a179d51
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 10:40:10 +0330

    Changed section to div

--- src/Components/Body/ShopLayout/ShopLayout.jsx
+++ src/Components/Body/ShopLayout/ShopLayout.jsx
@@ -7,11 +7,11 @@ function ShopLayout() {
     <section className="w-full mt-20 flex-center flex-col">
       <h2 className="font-bold text-white text-3xl text-center mb-4">چیدمان شگفت انگیز فروشگاه روی میز!</h2>
       <p className="text-white text-center text-sm">فروش مقرون به صرفه و عاقلانه با گزینه های داخلی فروشگاه الکترونیکی!</p>
-      <section className="max-w-[75rem] flex-center gap-6 flex-wrap mt-12">
+      <div className="max-w-[75rem] flex-center gap-6 flex-wrap mt-12">
         {data.map((item, index) => {
           return <NonButtonCard key={index} title="ساخت و ساز" image="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" />;
         })}
-      </section>
+      </div>
     </section>
   );
 }
