c1987c67b45aed11a0fa5f089f18317050af6d34
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 09:42:37 +0330

    headings have 1 rem margin

--- src/Components/Body/ShopLayout/ShopLayout.jsx
+++ src/Components/Body/ShopLayout/ShopLayout.jsx
@@ -5,7 +5,7 @@ function ShopLayout() {
   const data = [1, 2];
   return (
     <section className="w-full mt-20 flex-center flex-col">
-      <h2 className="font-bold text-white text-3xl text-center">چیدمان شگفت انگیز فروشگاه روی میز!</h2>
+      <h2 className="font-bold text-white text-3xl text-center mb-4">چیدمان شگفت انگیز فروشگاه روی میز!</h2>
       <p className="text-white text-center text-sm">فروش مقرون به صرفه و عاقلانه با گزینه های داخلی فروشگاه الکترونیکی!</p>
       <section className="max-w-[75rem] flex-center gap-6 flex-wrap mt-12">
         {data.map((item, index) => {
