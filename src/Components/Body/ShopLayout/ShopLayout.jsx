26887916c6e5986ddc07e2b8a5fef5979a92c280
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 09:35:47 +0330

    h3 changed to p

--- src/Components/Body/ShopLayout/ShopLayout.jsx
+++ src/Components/Body/ShopLayout/ShopLayout.jsx
@@ -4,7 +4,7 @@ function ShopLayout() {
   return (
     <section className="w-full mt-20 flex-center flex-col">
       <h2 className="font-bold text-white text-3xl text-center">چیدمان شگفت انگیز فروشگاه روی میز!</h2>
-      <h3 className="text-white text-center text-sm">فروش مقرون به صرفه و عاقلانه با گزینه های داخلی فروشگاه الکترونیکی!</h3>
+      <p className="text-white text-center text-sm">فروش مقرون به صرفه و عاقلانه با گزینه های داخلی فروشگاه الکترونیکی!</p>
     </section>
   );
 }
