8b9ee0a774b5439ea25f64a84c6c46c12876b54c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-07 08:40:21 +0330

    Catching props

--- src/Components/Utils/SectionTitle/SectionTitle.tsx
+++ src/Components/Utils/SectionTitle/SectionTitle.tsx
@@ -6,7 +6,8 @@ interface props {
     description: string;
 }
 
-function SectionTitle() {
+function SectionTitle(props: props) {
+    const {number, title, description} = props
   return (
     <div className="flex-center max-md:flex-col gap-4 px-4 mb-12">
     <h3 className="flex-center text-primary text-[5rem]">32</h3>
