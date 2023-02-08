08fda7b1652d4be6f95e5704d23df41a00b2ad12
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-08 10:16:15 +0330

    Added a heading

--- src/Components/Body/FullyEditable/FullyEditable.jsx
+++ src/Components/Body/FullyEditable/FullyEditable.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function FullyEditable() {
   return (
     <section className='py-20 flex-center flex-col w-full px-4 gap-4'>
-    
+    <h2 className='text-center text-3xl font-bold'>هدر و فوتر کاملاً قابل ویرایش</h2>
     </section>
   )
 }
