78bbe33a0f679d78a8b4d8d640e449722638b6d7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-08 10:30:47 +0330

    Interface is defined

--- src/Components/Body/UnifiedTheme/unifiedTheme.tsx
+++ src/Components/Body/UnifiedTheme/unifiedTheme.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-function unifiedTheme() {
+interface props {
+  title: string;
+  description: string;
+  image: string;
+}
+
+function unifiedTheme(props) {
   return (
     <section className={`${styles.wrapper} py-20 w-full flex-center flex-col gap-5 lg:min-h-[18.75rem] mt-20 px-4`}>
       <h2 className="mt-5 text-3xl font-bold text-secondary text-center">تم یکپارچه برای همه انواع مشاغل ساختمانی!</h2>
