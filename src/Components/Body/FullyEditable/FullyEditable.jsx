52792b24db4949df52122ce9c1d579a7549d43c6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-08 10:20:46 +0330

    Added the image

--- src/Components/Body/FullyEditable/FullyEditable.jsx
+++ src/Components/Body/FullyEditable/FullyEditable.jsx
@@ -6,6 +6,7 @@ function FullyEditable() {
     <section className="py-20 flex-center flex-col w-full px-4 gap-4">
       <h2 className="text-center text-3xl font-bold">هدر و فوتر کاملاً قابل ویرایش</h2>
       <p className="text-sm text-center">ایجاد هدر و فوتر با استفاده از رابط کاربری واضح و ساده!</p>
+      <img src={fullyEditable} alt="Fully Editable" />
     </section>
   );
 }
