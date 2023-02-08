e9ec4b08260c0527fc84d126c0f35526d3d00b3e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-08 10:22:36 +0330

    background is white

--- src/Components/Body/FullyEditable/FullyEditable.jsx
+++ src/Components/Body/FullyEditable/FullyEditable.jsx
@@ -3,10 +3,10 @@ import fullyEditable from "../../../assets/images/fullyEditable.webp";
 
 function FullyEditable() {
   return (
-    <section className="py-20 flex-center flex-col w-full px-4 gap-4">
-      <h2 className="text-center text-3xl font-bold">هدر و فوتر کاملاً قابل ویرایش</h2>
-      <p className="text-sm text-center">ایجاد هدر و فوتر با استفاده از رابط کاربری واضح و ساده!</p>
-      <img className="rounded-sm" src={fullyEditable} alt="Fully Editable" />
+    <section className="py-20 flex-center flex-col w-full bg-white px-4 gap-4">
+      <h2 className="text-center text-3xl font-bold text-secondary">هدر و فوتر کاملاً قابل ویرایش</h2>
+      <p className="text-sm text-center text-secondary">ایجاد هدر و فوتر با استفاده از رابط کاربری واضح و ساده!</p>
+      <img src={fullyEditable} alt="Fully Editable" />
     </section>
   );
 }
