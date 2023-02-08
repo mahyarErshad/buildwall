084cabddb80eb5006eedff5797c65b51c1304f4b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-08 09:06:47 +0330

    Headings are added

--- src/Components/Body/UnifiedTheme/unifiedTheme.jsx
+++ src/Components/Body/UnifiedTheme/unifiedTheme.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import styles from "./styles.module.css"
+import React from "react";
+import styles from "./styles.module.css";
 
 function unifiedTheme() {
   return (
-    <section className={`${styles.wrapper} py-20 w-full flex-center flex-col gap-5`}>
-
+    <section className={`${styles.wrapper} py-20 w-full flex-center flex-col gap-5 lg:min-h-[18.75rem] mt-20`}>
+      <h2 className="mt-5 text-3xl font-bold text-secondary">تم یکپارچه برای همه انواع مشاغل ساختمانی!</h2>
+      <h3 className="text-xl text-secondary">فروش مقرون به صرفه و عاقلانه با گزینه های فروشگاه الکترونیکی داخلی!</h3>
     </section>
-  )
+  );
 }
 
-export default unifiedTheme
+export default unifiedTheme;
