e958a05684180a4e143ff9713830e6a13bf61b24
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-08 10:32:48 +0330

    Using props to render component

--- src/Components/Body/UnifiedTheme/unifiedTheme.tsx
+++ src/Components/Body/UnifiedTheme/unifiedTheme.tsx
@@ -7,11 +7,11 @@ interface props {
   image: string;
 }
 
-function unifiedTheme(props) {
+function unifiedTheme(props: props) {
   return (
     <section className={`${styles.wrapper} py-20 w-full flex-center flex-col gap-5 lg:min-h-[18.75rem] mt-20 px-4`}>
-      <h2 className="mt-5 text-3xl font-bold text-secondary text-center">تم یکپارچه برای همه انواع مشاغل ساختمانی!</h2>
-      <h3 className="text-xl text-secondary text-center">فروش مقرون به صرفه و عاقلانه با گزینه های فروشگاه الکترونیکی داخلی!</h3>
+      <h2 className="mt-5 text-3xl font-bold text-secondary text-center"> {props.title} </h2>
+      <h3 className="text-xl text-secondary text-center"> {props.description} </h3>
     </section>
   );
 }
