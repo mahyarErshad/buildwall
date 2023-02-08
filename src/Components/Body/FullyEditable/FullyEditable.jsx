ab82b309ac3482210e106f5969ddaf99513dc8be
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-08 10:15:07 +0330

    Added a new image

--- src/Components/Body/FullyEditable/FullyEditable.jsx
+++ src/Components/Body/FullyEditable/FullyEditable.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FullyEditable() {
   return (
-    <section className='py-20 flex-center flex-col w-full px-4'>
+    <section className='py-20 flex-center flex-col w-full px-4 gap-4'>
     
     </section>
   )
