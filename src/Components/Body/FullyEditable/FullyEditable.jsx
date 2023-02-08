73d438eb35e5f26c47f564aa8bacdeb140f39439
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-08 10:17:30 +0330

    Added a p tag

--- src/Components/Body/FullyEditable/FullyEditable.jsx
+++ src/Components/Body/FullyEditable/FullyEditable.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function FullyEditable() {
   return (
-    <section className='py-20 flex-center flex-col w-full px-4 gap-4'>
-    <h2 className='text-center text-3xl font-bold'>هدر و فوتر کاملاً قابل ویرایش</h2>
+    <section className="py-20 flex-center flex-col w-full px-4 gap-4">
+      <h2 className="text-center text-3xl font-bold">هدر و فوتر کاملاً قابل ویرایش</h2>
+      <p className="text-sm text-center">ایجاد هدر و فوتر با استفاده از رابط کاربری واضح و ساده!</p>
     </section>
-  )
+  );
 }
 
-export default FullyEditable
+export default FullyEditable;
