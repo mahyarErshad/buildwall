8fc1730e01cff026f5d1518829f843e65d6fa00b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-08 09:07:35 +0330

    unifiedTheme is responsive

--- src/Components/Body/UnifiedTheme/unifiedTheme.jsx
+++ src/Components/Body/UnifiedTheme/unifiedTheme.jsx
@@ -3,9 +3,9 @@ import styles from "./styles.module.css";
 
 function unifiedTheme() {
   return (
-    <section className={`${styles.wrapper} py-20 w-full flex-center flex-col gap-5 lg:min-h-[18.75rem] mt-20`}>
-      <h2 className="mt-5 text-3xl font-bold text-secondary">تم یکپارچه برای همه انواع مشاغل ساختمانی!</h2>
-      <h3 className="text-xl text-secondary">فروش مقرون به صرفه و عاقلانه با گزینه های فروشگاه الکترونیکی داخلی!</h3>
+    <section className={`${styles.wrapper} py-20 w-full flex-center flex-col gap-5 lg:min-h-[18.75rem] mt-20 px-4`}>
+      <h2 className="mt-5 text-3xl font-bold text-secondary text-center">تم یکپارچه برای همه انواع مشاغل ساختمانی!</h2>
+      <h3 className="text-xl text-secondary text-center">فروش مقرون به صرفه و عاقلانه با گزینه های فروشگاه الکترونیکی داخلی!</h3>
     </section>
   );
 }
