6d7af0abff4bb11dfa46bd26598311caa5d58988
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-09 08:49:13 +0330

    id = store-pages

--- src/Components/Body/ShopLayout/ShopLayout.jsx
+++ src/Components/Body/ShopLayout/ShopLayout.jsx
@@ -4,7 +4,7 @@ import NonButtonCard from "../../Utils/NonButtonCard/NonButtonCard";
 function ShopLayout() {
   const data = [1, 2];
   return (
-    <section className="w-full mt-20 flex-center flex-col">
+    <section id="store-pages" className="w-full mt-20 flex-center flex-col">
       <h2 className="font-bold text-white text-3xl text-center mb-4">چیدمان شگفت انگیز فروشگاه روی میز!</h2>
       <p className="text-white text-center text-sm">فروش مقرون به صرفه و عاقلانه با گزینه های داخلی فروشگاه الکترونیکی!</p>
       <div className="max-w-[75rem] flex-center gap-6 flex-wrap mt-12">
