aff520827879568b96b12dfc2bdc08a1fe7303f6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-09 08:47:24 +0330

    id = custom-pages

--- src/Components/Body/CustomPages/CustomPages.jsx
+++ src/Components/Body/CustomPages/CustomPages.jsx
@@ -5,7 +5,7 @@ import NonButtonCard from "../../Utils/NonButtonCard/NonButtonCard";
 function CustomPages() {
   const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
-    <section className="w-full mt-20 flex-center flex-col">
+    <section id="custom-pages" className="w-full mt-20 flex-center flex-col">
       <SectionTitle number={30} title="صفحه سفارشی" description="عملکرد و تنوع غنی بیلدوال را کاوش کنید" />
       <section className="max-w-[75rem] flex-center gap-6 flex-wrap">
         {data.map((item, index) => {
