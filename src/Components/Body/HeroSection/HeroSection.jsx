d319f3dd34a42e4bf7fd3972454b2d33490d9e79
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-09 08:46:20 +0330

    id = home

--- src/Components/Body/HeroSection/HeroSection.jsx
+++ src/Components/Body/HeroSection/HeroSection.jsx
@@ -6,7 +6,7 @@ import { TypeAnimation } from "react-type-animation";
 
 function HeroSection() {
   return (
-    <section className={`${styles.wrapper} w-full h-full`}>
+    <section id="home" className={`${styles.wrapper} w-full h-full`}>
       <article className="mx-auto pt-20 flex-center flex-col gap-4">
         <TypeAnimation className="font-bold text-secondary text-3xl max-sm:h-20 sm:h-auto" sequence={["قالب شرکتی ساختمانی بیلدوال", 3000, "قالب یکپارچه برای انواع سایت های شرکتی!", 3000, , "قالب یکپارچه برای انواع مشاغل ساختمانی!", 3000]} speed={50} deletionSpeed={80} wrapper="h1" repeat={Infinity} />
         <p className="text-xl text-secondary">کسب و کار خود را با عملکرد باورنکردنی و رابط کاربری کارآمد متحول کنید!</p>
