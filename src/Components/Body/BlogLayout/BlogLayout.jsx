59a4f8c6ce8361538ee0377c89fc9f3c2eeeaa62
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-09 08:48:38 +0330

    id = blog-pages

--- src/Components/Body/BlogLayout/BlogLayout.jsx
+++ src/Components/Body/BlogLayout/BlogLayout.jsx
@@ -5,7 +5,7 @@ import NonButtonCard from "../../Utils/NonButtonCard/NonButtonCard";
 function BlogLayout() {
   const data = [1, 2, 3, 4];
   return (
-    <section className="w-full mt-20 flex-center flex-col">
+    <section id="blog-pages" className="w-full mt-20 flex-center flex-col">
       <SectionTitle number={4} title="طرح بندی زیبای وبلاگ!" description="اخبار و پست های وبلاگ خود را به روش های مختلف نشان دهید!" />
       <section className="max-w-[75rem] flex-center gap-6 flex-wrap">
         {data.map((item, index) => {
