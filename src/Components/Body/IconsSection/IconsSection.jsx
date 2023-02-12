567bf2448666e4d348530c04841457eeabbde784
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-12 12:56:52 +0330

    Changed icon names

--- src/Components/Body/IconsSection/IconsSection.jsx
+++ src/Components/Body/IconsSection/IconsSection.jsx
@@ -5,6 +5,9 @@ function IconsSection() {
     <section className="py-20 flex-center flex-col w-full bg-white px-4 gap-4">
       <h2 className="text-center text-3xl font-bold text-secondary">مجموعه بزرگی از آیکونهای فونت شکل</h2>
       <p className="text-sm text-center text-secondary"> وب سایت خود را با هزاران عنصر جذاب که مخصوص این تم ایجاد شده اند، تجهیز کنید! این آیکون های فونت شکل به عنوان یک گیلاس روی شیرینی در وب سایت شما خواهند بود! </p>
+      <div className="max-w-[75rem] flex-center gap-4 flex-wrap">
+      
+      </div>
     </section>
   );
 }
