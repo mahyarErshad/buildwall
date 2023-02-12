3334bc80bb0669647cadd95b38557153bb2bdb59
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-12 12:39:54 +0330

    First card is created

--- src/Components/Body/ShopLayout/ShopLayout.jsx
+++ src/Components/Body/ShopLayout/ShopLayout.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import NonButtonCard from "../../Utils/NonButtonCard/NonButtonCard";
+import listingShop from "../../../assets/images/card-images/listingShop.webp"
 
 function ShopLayout() {
-  const data = [1, 2];
+  const data = [
+    {
+      title: "کلکسیون",
+      image: listingShop,
+      href: "https://ld-wp73.template-help.com/buildwall/landing/",
+    },
+    {
+      title: "کلکسیون",
+      image: listingShop,
+      href: "https://ld-wp73.template-help.com/buildwall/landing/",
+    },
+  ];
   return (
     <section id="store-pages" className="w-full mt-20 flex-center flex-col">
       <h2 className="font-bold text-white text-3xl text-center mb-4">چیدمان شگفت انگیز فروشگاه روی میز!</h2>
       <p className="text-white text-center text-sm">فروش مقرون به صرفه و عاقلانه با گزینه های داخلی فروشگاه الکترونیکی!</p>
       <div className="max-w-[75rem] flex-center gap-6 flex-wrap mt-12">
         {data.map((item, index) => {
-          return <NonButtonCard key={index} title="ساخت و ساز" image="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" />;
+          return <NonButtonCard key={index} title={item.title} image={item.image} href={item.href} />;
         })}
       </div>
     </section>
