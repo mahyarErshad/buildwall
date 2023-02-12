09156bab1d75c161e0486dae31090c9ade9a32eb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-12 12:54:12 +0330

    Added title and description

--- src/Components/Body/IconsSection/IconsSection.jsx
+++ src/Components/Body/IconsSection/IconsSection.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function IconsSection() {
   return (
-    <div>IconsSection</div>
-  )
+    <section className="py-20 flex-center flex-col w-full bg-white px-4 gap-4">
+      <h2 className="text-center text-3xl font-bold text-secondary">مجموعه بزرگی از آیکونهای فونت شکل</h2>
+      <p className="text-sm text-center text-secondary"> وب سایت خود را با هزاران عنصر جذاب که مخصوص این تم ایجاد شده اند، تجهیز کنید! این آیکون های فونت شکل به عنوان یک گیلاس روی شیرینی در وب سایت شما خواهند بود! </p>
+    </section>
+  );
 }
 
-export default IconsSection
+export default IconsSection;
