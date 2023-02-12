2db1373ce58ccdacd078296c80c8418d513fd544
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-12 11:49:27 +0330

    Cards are real

--- src/Components/Body/BlogLayout/BlogLayout.jsx
+++ src/Components/Body/BlogLayout/BlogLayout.jsx
@@ -3,13 +3,19 @@ import SectionTitle from "../../Utils/SectionTitle/SectionTitle";
 import NonButtonCard from "../../Utils/NonButtonCard/NonButtonCard";
 
 function BlogLayout() {
-  const data = [1, 2, 3, 4];
+  const data = [
+    {
+      title: "ساخت و ساز",
+      image: "",
+      href: "https://ld-wp73.template-help.com/buildwall/v3/default/",
+    },
+  ];
   return (
     <section id="blog-pages" className="w-full mt-20 flex-center flex-col">
       <SectionTitle number={4} title="طرح بندی زیبای وبلاگ!" description="اخبار و پست های وبلاگ خود را به روش های مختلف نشان دهید!" />
       <section className="max-w-[75rem] flex-center gap-6 flex-wrap">
         {data.map((item, index) => {
-          return <NonButtonCard key={index} title="ساخت و ساز" image="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" />;
+          return <NonButtonCard key={index} title={item.title} image={item.image} href={item.href} />;
         })}
       </section>
     </section>
