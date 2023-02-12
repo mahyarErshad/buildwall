e8926e7d2f01800642619747d2c5e602c6aa0002
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-12 11:16:00 +0330

    Another card is created

--- src/Components/Body/CustomPages/CustomPages.jsx
+++ src/Components/Body/CustomPages/CustomPages.jsx
@@ -7,7 +7,8 @@ import about3 from "../../../assets/images/card-images/about3.webp";
 import service1 from "../../../assets/images/card-images/service1.webp";
 import service2 from "../../../assets/images/card-images/service2.webp";
 import service3 from "../../../assets/images/card-images/service3.webp";
-import serviceSingle1 from "../../../assets/images/card-images/serviceSingle1.webp";
+import serviceSingle1 from "../../../assets/images/card-images/serviceSingle1.webp"; 
+import serviceSingle2 from "../../../assets/images/card-images/serviceSingle2.webp"; 
 
 function CustomPages() {
   const data = [
@@ -45,6 +46,11 @@ function CustomPages() {
       title: "خدمات تکی 1",
       image: serviceSingle1,
       href: "https://ld-wp73.template-help.com/buildwall/v3/default/services/construction-consultation/",
+    },
+    {
+      title: "خدمات تکی 2",
+      image: serviceSingle2,
+      href: "https://ld-wp73.template-help.com/buildwall/v3/default/services/house-renovation/",
     },
   ];
   return (
