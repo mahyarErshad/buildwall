6a99335720e88d5535fb05219f17a63afdd8da75
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-12 10:58:26 +0330

    Cards are real

--- src/Components/Body/DemosSection/DemosSection.jsx
+++ src/Components/Body/DemosSection/DemosSection.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import DemoSectionCard from "./DemosSectionCard";
 import SectionTitle from "../../Utils/SectionTitle/SectionTitle";
+import image1 from "../../../assets/images/card-images/skin1.webp";
+import image2 from "../../../assets/images/card-images/skin2.webp";
+import image3 from "../../../assets/images/card-images/skin3.webp";
 
 function DemosSection() {
-  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const data = [
+    {
+      title: "ساخت و ساز",
+      image: image1,
+      href: "https://ld-wp73.template-help.com/buildwall/v3/default/",
+    },
+    {
+      title: "باغبانی و منظره",
+      image: image2,
+      href: "https://ld-wp73.template-help.com/buildwall/v2/gardening/",
+    },
+    {
+      title: "استودیو معماری و طراحی داخلی مدرن",
+      image: image3,
+      href: "https://ld-wp73.template-help.com/buildwall/v2/architecture-2/",
+    },
+  ];
   return (
     <section id="demos" className="max-md:mt-40 max-lg:mt-52 lg:mt-[17rem] w-full flex-center flex-col">
       <SectionTitle number="32" title="دموی شگفت انگیز در انتظار شما هستند!" description="پوسته های منحصر به فرد را برای چندین موضوع ساخت و ساز در این یک موضوع پیدا کنید!" />
       <section className="w-full flex-center gap-6 flex-wrap">
         {data.map((item, index) => {
-          return <DemoSectionCard key={index} title="ساخت و ساز" image="https://ld-wp73.template-help.com/buildwall/landing/images/skin-1.png" />;
+          return <DemoSectionCard key={index} title={item.title} image={item.image} href={item.href} />;
         })}
       </section>
     </section>
--- src/Components/Body/DemosSection/DemosSectionCard.tsx
+++ src/Components/Body/DemosSection/DemosSectionCard.tsx
@@ -4,6 +4,7 @@ import NeonButton from "../../Utils/NeonButton/NeonButton";
 interface props {
   image: string;
   title: string;
+  href: string;
 }
 
 function DemosSectionCard(props: props) {
@@ -11,7 +12,7 @@ function DemosSectionCard(props: props) {
     <figure data-aos="zoom-in" className="flex-center flex-col gap-4 max-md:mb-8 md:mb-4 max-md:w-full md:w-[40%] lg:w-1/4">
       <img src={props.image} alt={props.title} />
       <h2 className="mb-4">{props.title}</h2>
-      <NeonButton />
+      <NeonButton href={props.href} target="_blank" />
     </figure>
   );
 }
