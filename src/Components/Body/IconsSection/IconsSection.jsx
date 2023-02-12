c490b794e3bd18f7dd20508ca44da2886ee48af3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-12 13:08:35 +0330

    Card has an animation

--- src/Components/Body/IconsSection/IconsSection.jsx
+++ src/Components/Body/IconsSection/IconsSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./styles.module.css";
 import icon1 from "../../../assets/images/icons/1.png";
 
 function IconsSection() {
@@ -7,8 +8,8 @@ function IconsSection() {
       <h2 className="text-center text-3xl font-bold text-secondary">مجموعه بزرگی از آیکونهای فونت شکل</h2>
       <p className="text-sm text-center text-secondary"> وب سایت خود را با هزاران عنصر جذاب که مخصوص این تم ایجاد شده اند، تجهیز کنید! این آیکون های فونت شکل به عنوان یک گیلاس روی شیرینی در وب سایت شما خواهند بود! </p>
       <div className="max-w-[75rem] flex-center gap-4 flex-wrap">
-        <figure className="w-[6.8rem] h-[6.8rem] border border-[#e9e9f0] flex-center bg-white hover:bg-primary transition-all duration-300">
-          <img className="h-[4.375rem] w-[4.375rem]" src={icon1} alt="" />
+        <figure className={`w-[6.8rem] h-[6.8rem] border border-[#e9e9f0] flex-center bg-white hover:bg-primary transition-all duration-500 ${styles.card}`}>
+          <img className="h-[4.375rem] w-[4.375rem] transition-all duration-150" src={icon1} alt="" />
         </figure>
       </div>
     </section>
