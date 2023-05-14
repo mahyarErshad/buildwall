1aed1d875b94fa5b51f3d256697a12741eab95d4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-14 14:26:02 +0330

    Removed 2 demos

--- src/Components/Body/DemosSection/DemosSection.jsx
+++ src/Components/Body/DemosSection/DemosSection.jsx
@@ -8,24 +8,14 @@ import image3 from "../../../assets/images/card-images/skin3.webp";
 function DemosSection() {
   const data = [
     {
-      title: "باغبانی و منظره",
-      image: image2,
-      href: "https://ld-wp73.template-help.com/buildwall/v2/gardening/",
-    },
-    {
       title: "ساخت و ساز",
       image: image1,
       href: "https://ld-wp73.template-help.com/buildwall/v3/default/",
     },
-    {
-      title: "استودیو معماری و طراحی داخلی مدرن",
-      image: image3,
-      href: "https://ld-wp73.template-help.com/buildwall/v2/architecture-2/",
-    },
   ];
   return (
     <section id="demos" className="max-md:mt-40 max-lg:mt-52 lg:mt-[17rem] w-full flex-center flex-col">
-      <SectionTitle number="3" title="دموی شگفت انگیز در انتظار شماست!" description="پوسته های منحصر به فرد را برای چندین موضوع ساخت و ساز در این یک موضوع پیدا کنید!" />
+      <SectionTitle title="این دموی شگفت انگیز در انتظار شماست!" description="پوسته های منحصر به فرد را برای چندین موضوع ساخت و ساز در این یک موضوع پیدا کنید!" />
       <section className="w-full flex-center gap-6 flex-wrap">
         {data.map((item, index) => {
           return <DemoSectionCard key={index} title={item.title} image={item.image} href={item.href} />;
